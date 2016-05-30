fcf5e2ddf966f0acdbf5fe4a5aeb4affd6ae4d0a
Tomas Korcak <korczis@gmail.com>
2016-05-31 01:46:21 +0200

    Cosmetics only

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,13 +1,13 @@
 // pub use self::greetings::hello;
 
 mod account_setting;
-mod post_user;
+mod post_user_login;
 mod project;
 mod projects;
 mod user_login;
 
 pub use self::account_setting::*;
-pub use self::post_user::*;
+pub use self::post_user_login::*;
 // pub use self::project::*;
 pub use self::projects::*;
 pub use self::user_login::*;
