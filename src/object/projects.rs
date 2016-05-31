007290013b0152cd01ccec5a3ebd7350fbff5865
Tomas Korcak <korczis@gmail.com>
2016-05-31 19:49:32 +0200

    Into string trait implemented

--- src/object/projects.rs
+++ src/object/projects.rs
@@ -1,3 +1,5 @@
+use rustc_serialize::json;
+
 pub use object::project::*;
 
 #[derive(RustcDecodable, RustcEncodable)]
@@ -5,8 +7,8 @@ pub struct Projects {
     pub projects: Vec<Project>,
 }
 
-// impl Projects {
-//     fn project(&self, idx: usize) -> &Project {
-//         &self.projects[idx]
-//     }
-// }
+impl Into<String> for Projects {
+    fn into(self) -> String {
+        format!("{}\n", json::as_pretty_json(&self).to_string())
+    }
+}
--- src/object/user_login.rs
+++ src/object/user_login.rs
@@ -1,11 +1,25 @@
+use rustc_serialize::json;
+
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct UserLoginBody {
     pub profile: String,
     pub state: String,
 }
 
+impl Into<String> for UserLoginBody {
+    fn into(self) -> String {
+        format!("{}\n", json::as_pretty_json(&self).to_string())
+    }
+}
+
 #[allow(non_snake_case)]
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct UserLogin {
     pub userLogin: UserLoginBody,
+}
+
+impl Into<String> for UserLogin {
+    fn into(self) -> String {
+        format!("{}\n", json::as_pretty_json(&self).to_string())
+    }
 }
