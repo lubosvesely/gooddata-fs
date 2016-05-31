ede8926677ed2220aa69b6d36d5431df2b7025ce
Tomas Korcak <korczis@gmail.com>
2016-05-31 19:48:16 +0200

    Create helpers module

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -2,9 +2,10 @@ extern crate rand;
 
 use rand::Rng;
 
-pub fn random_string() -> String {
+pub fn random_string(count: usize) -> String {
     let rstr: String = rand::thread_rng()
         .gen_ascii_chars()
-        .take(32)
+        .take(count)
         .collect();
+    return rstr;
 }
