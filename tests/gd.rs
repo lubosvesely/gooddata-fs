d3b438c4bbfceab42da72ce38e2d98d2405ebc34
Tomas Korcak <korczis@gmail.com>
2016-06-02 16:49:21 +0200

    Test for projects_fetch()

--- tests/gd.rs
+++ tests/gd.rs
@@ -34,3 +34,15 @@ fn client_can_disconnect() {
     assert_eq!(gd.user.is_some(), false);
     assert_eq!(gd.token_updated.is_some(), false);
 }
+
+#[test]
+fn client_can_get_projects() {
+    let mut gd = connect();
+    gd.projects_fetch();
+
+    assert_eq!(gd.projects.is_some(), true);
+    assert_eq!(gd.user.is_some(), true);
+    assert_eq!(gd.token_updated.is_some(), true);
+
+    gd.disconnect();
+}
