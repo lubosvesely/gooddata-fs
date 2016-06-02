ab6ffd9b5171396b5c811acaf1e2fd4563ad221b
Tomas Korcak <korczis@gmail.com>
2016-06-02 16:17:00 +0200

    Refactor to lib and main

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate log;
+
+extern crate chrono;
+extern crate cookie;
+extern crate core;
+extern crate env_logger;
+extern crate fuse;
+extern crate hyper;
+extern crate libc;
+extern crate rand;
+extern crate regex;
+extern crate rustc_serialize;
+extern crate time;
+extern crate users;
+
+pub mod fs;
+pub mod gd;
+pub mod helpers;
+pub mod object;
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,12 @@
-
-#[macro_use]
-extern crate log;
-
-extern crate chrono;
-extern crate cookie;
-extern crate core;
-extern crate env_logger;
-extern crate fuse;
-extern crate hyper;
-extern crate libc;
-extern crate rand;
-extern crate regex;
-extern crate rustc_serialize;
-extern crate time;
+extern crate gooddata_fs;
 extern crate users;
 
 use std::env;
 
-mod fs;
-mod gd;
-mod helpers;
-mod object;
+use gooddata_fs::*;
 
 fn main() {
-    let mut gd = gd::GoodDataClient::new();
+    let mut gd = gooddata_fs::gd::GoodDataClient::new();
     let username = env::args_os().nth(1).unwrap();
     let password = env::args_os().nth(2).unwrap();
     gd.connect(username.to_str().unwrap().to_string(),
