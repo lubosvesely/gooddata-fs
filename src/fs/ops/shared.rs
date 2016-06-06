042f592d73f7c8d1f33892fe1fab6cce5c0deee4
Tomas Korcak <korczis@gmail.com>
2016-06-07 00:27:24 +0200

    Naming cosmetics

--- src/fs/ops/shared.rs
+++ src/fs/ops/shared.rs
@@ -3,11 +3,6 @@ use fuse::FileType;
 use fs::constants;
 use fs::flags;
 
-// Category
-// ID ???
-// Type
-// Filename/Dirname
-
 pub struct ProjectItem {
     pub category: u8,
     pub reserved: u8,
@@ -15,51 +10,51 @@ pub struct ProjectItem {
     pub path: &'static str,
 }
 
-pub const ITEM_PROJECT_FEATURE_FLAGS_JSON: ProjectItem = ProjectItem {
+pub const PROJECT_FEATURE_FLAGS_JSON: ProjectItem = ProjectItem {
     category: flags::Category::Internal as u8,
     reserved: flags::ReservedFile::FeatureFlagsJson as u8,
     item_type: FileType::RegularFile,
     path: constants::FEATURE_FLAGS_JSON_FILENAME,
 };
 
-pub const ITEM_PROJECT_PERMISSIONS_JSON: ProjectItem = ProjectItem {
+pub const PROJECT_PERMISSIONS_JSON: ProjectItem = ProjectItem {
     category: flags::Category::Internal as u8,
     reserved: flags::ReservedFile::PermissionsJson as u8,
     item_type: FileType::RegularFile,
     path: constants::USER_PERMISSIONS_JSON_FILENAME,
 };
 
-pub const ITEM_PROJECT_JSON: ProjectItem = ProjectItem {
+pub const PROJECT_JSON: ProjectItem = ProjectItem {
     category: flags::Category::Internal as u8,
     reserved: flags::ReservedFile::ProjectJson as u8,
     item_type: FileType::RegularFile,
     path: constants::PROJECT_JSON_FILENAME,
 };
 
-pub const ITEM_USER_ROLES_JSON: ProjectItem = ProjectItem {
+pub const USER_ROLES_JSON: ProjectItem = ProjectItem {
     category: flags::Category::Ldm as u8,
     reserved: flags::ReservedFile::RolesJson as u8,
     item_type: FileType::RegularFile,
     path: constants::USER_ROLES_JSON_FILENAME,
 };
 
-pub const ITEM_LDM_DIR: ProjectItem = ProjectItem {
+pub const LDM_DIR: ProjectItem = ProjectItem {
     category: flags::Category::Internal as u8,
     reserved: flags::ReservedFile::KeepMe as u8,
     item_type: FileType::Directory,
     path: constants::PROJECT_LDM_DIR,
 };
 
-pub const ITEM_METADATA_DIR: ProjectItem = ProjectItem {
+pub const METADATA_DIR: ProjectItem = ProjectItem {
     category: flags::Category::Metadata as u8,
     reserved: flags::ReservedFile::KeepMe as u8,
     item_type: FileType::Directory,
     path: constants::PROJECT_METADATA_DIR,
 };
 
-pub const ITEMS: [ProjectItem; 6] = [ITEM_PROJECT_FEATURE_FLAGS_JSON,
-                                     ITEM_PROJECT_PERMISSIONS_JSON,
-                                     ITEM_PROJECT_JSON,
-                                     ITEM_USER_ROLES_JSON,
-                                     ITEM_LDM_DIR,
-                                     ITEM_METADATA_DIR];
+pub const ITEMS: [ProjectItem; 6] = [PROJECT_FEATURE_FLAGS_JSON,
+                                     PROJECT_PERMISSIONS_JSON,
+                                     PROJECT_JSON,
+                                     USER_ROLES_JSON,
+                                     LDM_DIR,
+                                     METADATA_DIR];
