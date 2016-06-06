af9a004c71a8aa0003e82e8f66333a3cc201f679
Tomas Korcak <korczis@gmail.com>
2016-06-07 01:26:23 +0200

    Use mountpoint from clap-rs options

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate gooddata_fs;
 extern crate users;
 
 use clap::{Arg, App};
-use std::env;
 
 use gooddata_fs::*;
 
@@ -11,6 +10,7 @@ const DESCRIPTION: &'static str = "GoodData as Filesystem"; // env!("CARGO_PKG_D
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
+    // Specify program options
     let matches = App::new(DESCRIPTION)
         .version(VERSION)
         .arg(Arg::with_name("server")
@@ -35,18 +35,22 @@ fn main() {
             .index(3))
         .get_matches();
 
+    // Parse required program options
     let username = matches.value_of("username").unwrap().to_string();
     let password = matches.value_of("password").unwrap().to_string();
+    let mountpoint = matches.value_of("mountpoint").unwrap().to_string();
     let server = matches.value_of("server").unwrap().to_string();
 
+    // Create instance of GoodData REST API Client
     let mut gd = gooddata_fs::gd::GoodDataClient::new(server);
     gd.connect(username, password);
 
-    // Mount GoodData
-    let mountpoint = env::args_os().nth(3).unwrap();
+    // Create GoodData Filesystem instance
     let fs = fs::GoodDataFS {
         client: gd,
         users_cache: users::UsersCache::new(),
     };
-    fs.mount(mountpoint.to_str().unwrap().to_string());
+
+    // Mount GoodData Filesystem
+    fs.mount(mountpoint);
 }
